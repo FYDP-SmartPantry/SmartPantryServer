6fc0dd801b0afe43258be445f8eedf56d35eb455
omegacoustic <43961560+omegacoustic@users.noreply.github.com>
2021-02-25 16:19:04 -0500

    Add files via upload

--- food_prediction.py
+++ food_prediction.py
@@ -81,7 +81,3 @@ def name_rectify(d_result):
             l_r_name.append(r_name)
 
     return if_food, l_r_name
-
-
-if __name__ == "__main__":
-    food_dect("/home/ubuntu/SmartPantryServer/apple.jpg","/home/ubuntu/SmartPantryServer/idenprof_061-0.7933.h5")
